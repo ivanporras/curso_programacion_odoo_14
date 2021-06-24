bda4faa155d1732e64695a6c9f8e9b544ff2873c
ivanporras <ivanporraslopez@gmail.com>
2021-06-24 12:54:32 -0500

    Primer modulo

---
+++ primer_modulo/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Curso de programacion Odoo 14",
+    'summary':"""
+        Curso de programacion Odoo 14, funcionalidades
+    """,
+    'author':'Ivan Porras',
+    'category':'General',
+    'version':'1.0.0',
+    'depends':[],
+    'data':[],
+}
